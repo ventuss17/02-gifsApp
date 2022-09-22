65f428fa8fca41414b7ae449a2cbca086eccbb2a
Nicolas Venturini <ventuss17@gmail.com>
2022-09-22 10:53:53 -0300

    fix: fin GifsApp

---
+++ src/app/gifs/gifs.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { GifsPageComponent } from './gifs-page/gifs-page.component';
+import { SearchComponent } from './search/search.component';
+import { ResultsComponent } from './results/results.component';
+
+
+
+@NgModule({
+  declarations: [
+    GifsPageComponent,
+    SearchComponent,
+    ResultsComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports:[
+    GifsPageComponent,
+    SearchComponent,
+
+  ]
+})
+export class GifsModule { }
---
+++ src/app/gifs/results/results.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { GifsService } from '../services/gifs.service';
+
+@Component({
+  selector: 'app-results',
+  templateUrl: './results.component.html',
+})
+export class ResultsComponent {
+  get resultados(){
+    return this.gifsService.resultados;
+  }
+
+  constructor(private gifsService:GifsService) { }
+
+
+}
